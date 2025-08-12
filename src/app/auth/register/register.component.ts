6a15bdeecff60a9408c078372bf995b15721a900
Shani Ben-Yosef <shani26018@gmail.com>
2025-08-12 09:52:14 +0300

    add success dialog and other css fixes

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, OnDestroy } from "@angular/core";
+import { Component, inject, OnDestroy, ViewChild, TemplateRef } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MatDialog } from '@angular/material/dialog';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import * as AuthActions from "../../state/auth/auth.actions";
 import { Store } from "@ngrx/store";
@@ -15,20 +16,24 @@ import { Subject, takeUntil } from "rxjs";
 @Component({
     standalone: true,
     selector: 'app-register',
-    imports: [MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule, CommonModule, MatProgressSpinnerModule],
+    imports: [MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule, CommonModule, MatProgressSpinnerModule, MatDialogModule],
     styleUrls: ['./register.component.css'],
     templateUrl: './register.component.html'
 })
 export class RegisterComponent implements OnDestroy {
-  private onDestroy$ = new Subject<void>();
+  @ViewChild('successDialog') successDialog!: TemplateRef<any>;
 
+  private onDestroy$ = new Subject<void>();
   private store = inject(Store);
+
+  dialogRef: any;
   registrationForm: FormGroup;
-  registrationMessage: string | undefined;
+  errorMessage: string | undefined;
+  successMessage: string | undefined;
   isDisabled = false;
   isLoading = false;
 
-  constructor(private formBuilder: FormBuilder, private actions$: Actions, private router: Router) {
+  constructor(private formBuilder: FormBuilder, private actions$: Actions, private router: Router, private dialog: MatDialog) {
     this.registrationForm = this.formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
@@ -40,14 +45,17 @@ export class RegisterComponent implements OnDestroy {
     this.actions$
       .pipe(ofType(AuthActions.registrationSuccess), takeUntil(this.onDestroy$))
       .subscribe(() => {
+        this.setSuccessDialog();
+
         setTimeout(() => {
+          this.dialogRef.close();
           this.router.navigate(['/login']);
         }, 1500);
       });
 
     this.actions$
       .pipe(ofType(AuthActions.registrationFailure), takeUntil(this.onDestroy$))
-      .subscribe((action) => (this.registrationMessage = action.error));
+      .subscribe((action) => (this.errorMessage = action.error));
   }
 
   ngOnDestroy(): void {
@@ -57,25 +65,35 @@ export class RegisterComponent implements OnDestroy {
 
   register() {
     if (!this.registrationForm.valid) {
-      this.registrationMessage = "יש למלא את כל השדות";
+      this.errorMessage = "יש למלא את כל השדות";
     }
     else {
       if (this.registrationForm.value.password !== this.registrationForm.value.passwordConfirmation) {
-        this.registrationMessage = "סיסמאות לא תואמות";
+        this.errorMessage = "סיסמאות לא תואמות";
       }
       else {
         const { passwordConfirmation: passwordConfirmation, ...plainedUser } = this.registrationForm.value;
         const user: User = plainedUser;
         this.store.dispatch(AuthActions.registrationRequest({ user }));
-
-        this.isLoading = true;
-        this.isDisabled = true;
-        this.registrationMessage = "נרשמת בהצלחה למערכת!";
       }
     }
   }
 
   openLoginPage() {
     this.router.navigate(['/login']);
+  }
+
+  setSuccessDialog() {
+    this.errorMessage = undefined;
+
+    this.isLoading = true;
+    this.isDisabled = true;
+    this.successMessage = "נרשמת בהצלחה למערכת!";
+
+    this.dialogRef = this.dialog.open(this.successDialog, {
+      data: { message: this.successMessage },
+      width: '320px',
+      panelClass: 'custom-success-dialog'
+    });
   }
 }
