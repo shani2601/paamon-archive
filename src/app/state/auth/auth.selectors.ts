8269b489903e9e02e784d60dc20b31c83b149fdc
Shani Ben-Yosef <shani26018@gmail.com>
2025-08-22 12:51:35 +0300

    fixes

--- src/app/state/auth/auth.selectors.ts
+++ src/app/state/auth/auth.selectors.ts
@@ -1,12 +1,13 @@
 import { createFeatureSelector, createSelector } from "@ngrx/store";
 import { AuthState } from "./auth.reducer";
 
-export const selectAuth = createFeatureSelector<AuthState>('auth');
+const selectAuth = createFeatureSelector<AuthState>('auth');
+
 export const selectUser = createSelector(selectAuth, store => store.user);
+export const selectIsLoggedIn = createSelector(selectUser, user => !!user);
 
 export const selectLoginLoading = createSelector(selectAuth, store => store.login.loading);
 export const selectLoginError = createSelector(selectAuth, store => store.login.error);
-export const selectIsLoggedIn = createSelector(selectUser, user => !!user);
 
 export const selectRegistrationLoading = createSelector(selectAuth, store => store.register.loading);
 export const selectRegistrationError = createSelector(selectAuth, store => store.register.error);
