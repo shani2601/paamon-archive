4e2604c64c7afae75194003fc7cd13ac9bc755cf
Shani Ben-Yosef <shani26018@gmail.com>
2025-08-24 20:29:22 +0300

    create a guard for home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { HomeComponent } from './main/home/home.component';
+import { authGuard } from './state/auth/auth.guard';
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
-    {path: 'home', component: HomeComponent}
+    {path: 'home', component: HomeComponent, canActivate: [authGuard]}
 ];
