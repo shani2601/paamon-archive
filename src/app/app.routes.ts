a2035b72ce6a19341fa153e1928952f4f8832ff0
Shani Ben-Yosef <shani26018@gmail.com>
2025-08-24 20:43:07 +0300

    fix paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { HomeComponent } from './main/home/home.component';
+import { LoginComponent } from '../app/components/auth/login/login.component';
+import { RegisterComponent } from '../app/components/auth/register/register.component';
+import { HomeComponent } from '../app/components/main/home/home.component';
 import { authGuard } from './state/auth/auth.guard';
 
 export const routes: Routes = [
